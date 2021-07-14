c6fe086309cc4184782e1b0962e1414d27641776
tailred <callum@twintailsare.moe>
2021-07-14 23:14:13 +0100

    Bugfixes in backup script

--- scripts/backup.py
+++ scripts/backup.py
@@ -37,12 +37,13 @@ class B2Connection:
 
         # Upload file to B2
         remote_file_name = f'{backup_prefix}/{datetime.isoformat(t)}'
+        print(f"Uploading database dump to {remote_file_name} on B2")
         self.bucket.upload_local_file(
             local_file=local_file_path,
             file_name=remote_file_name,
             file_infos={
                 "backup_type": backup_prefix,
-                "backup_timestamp": t.timestamp()
+                "backup_timestamp": str(t.timestamp())
             }
         )
 
@@ -51,12 +52,15 @@ class B2Connection:
 
     def remove_old_backups(self, backup_prefix: str, limit: int = 1):
         # Get list of existing backups
+        print("Checking for old backups to remove...")
         old_backups = list(self.bucket.ls(backup_prefix))
         if len(old_backups) > limit:
             # We have more backups stored than is allowed, so work out the difference and delete the oldest
             no_to_delete: int = len(old_backups) - limit
+            print(f"{no_to_delete} backups will now be removed")
             old_backups.sort(key=lambda b: b[0].file_info["backup_timestamp"])
             for file_to_delete in old_backups[:no_to_delete]:
+                print(f"Deleting backup {file_to_delete.file_name}")
                 self.b2api.delete_file_version(
                     file_to_delete.id_, file_to_delete.file_name)
 
@@ -76,8 +80,11 @@ if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv, "dw")
     for opt in opts:
         if opt == "-d":
+            print(f"Taking a daily backup of the DB cluster at {cluster_addr}")
             conn.backup_to_b2(cluster_addr, "daily")
             conn.remove_old_backups("daily", daily_backups_cnt)
         elif opt == "-w":
+            print(
+                f"Taking a weekly backup of the DB cluster at {cluster_addr}")
             conn.backup_to_b2(cluster_addr, "weekly")
             conn.remove_old_backups("weekly", weekly_backups_cnt)
